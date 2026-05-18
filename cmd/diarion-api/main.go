2d57c3419ed8fe0edbe780fdc7c1e15fa2b2f833
Sergey Subbotin <ssubbotin@gmail.com>
2026-05-18 17:47:55 +0200

    Add diarion-api stub with /healthz and /readyz

---
+++ cmd/diarion-api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(30 * time.Second))
+
+	r.Get("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	r.Get("/readyz", func(w http.ResponseWriter, _ *http.Request) {
+		// M1 will plumb actual DB + Redis checks.
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ready"))
+	})
+
+	addr := os.Getenv("API_LISTEN")
+	if addr == "" {
+		addr = ":8080"
+	}
+	slog.Info("diarion-api starting", "addr", addr)
+	if err := http.ListenAndServe(addr, r); err != nil {
+		slog.Error("diarion-api stopped", "err", err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/diarion/diarion-core
 
 go 1.24
+
+require github.com/go-chi/chi/v5 v5.2.5 // indirect
